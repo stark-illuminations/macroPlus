3e744664e2fa0d285a0caa1d07b8d3e0d1771c89
stark-illuminations <starkwinter129@gmail.com>
2025-02-21 09:04:20 -0600

    Changed the style of macro buttons and added selected button indicator

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,12 +1,20 @@
 function changeMacroEditor(uuid) {
     const macroEditor = document.getElementById("macro_editor")
+    const macroList = document.getElementById("macro_list")
     for (const child of macroEditor.children) {
         if (child.nodeName == "DIV" && !(child.classList.contains("hidden"))) {
             child.classList.add("hidden")
         }
         if (child.id.includes(uuid)) {
             child.classList.remove("hidden")
-
+        }
+    }
+    for (const child of macroList.children) {
+        if (child.nodeName == "DIV" && child.classList.contains("selected")) {
+            child.classList.remove("selected")
+        }
+        if (child.id.includes(uuid)) {
+            child.classList.add("selected")
         }
     }
 }
